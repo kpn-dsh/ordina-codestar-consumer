933a5fa40b26d5d86d71e16cdb0f5b3b7a8757fe
Juriaan Manuela <juriaan.manuela@kpn.com>
2022-04-01 07:14:30 +0200

    first commit

---
+++ README.md
@@ -0,0 +1,36 @@
+# Wat heb je nodig ?
+
+- Toegang tot dsh console           -> https://console.poc.kpn-dsh.com
+- Toegang tot de docker image repo  -> https://registry.cp.kpn-dsh.com/
+- Docker                            -> https://hub.docker.com/_/hello-world
+- Make                              -> https://chocolatey.org/install en vervolgens voer uit 
+choco install make  
+- Voorbeeld config een nieuwe service op de DSH
+```bash
+{
+	"name": "consumer-group-1",
+	"image": "registry.cp.kpn-dsh.com/ordina-codestar-2/transformer:1",
+	"cpus": 0.1,
+	"mem": 256,
+	"env": {
+		"KAFKA_TOPIC_FROM": "internal.gecombineerd-ordina-codestar-1"
+	},
+	"instances": 1,
+	"singleInstance": false,
+	"needsToken": true,
+	"user": "2037:2037"
+}
+```
+
+# 1 ) Opdracht uitvoeren
+- a. Pas de groepsnummer aan in de makefile
+- b. Print de events uit de stream "internal.gecombineerd-ordina-codestar-1" , zie https://kafka.js.org/docs/getting-started
+- c. Maak een service aan de DSH met de naam "consumer-group-1" <--- voeg de juiste groepsnummer toe
+
+- *bij elke aanpassing wordt geacht om de wijzigingen te pushen en te testen op de DSH (make all) & services restarten op de DSH*
+
+# 2 ) Nieuwe image bakken en pushen.
+```sh
+command : docker login registry.cp.kpn-dsh.com
+command : make all
+```
---
+++ index.js
@@ -0,0 +1,65 @@
+const { Kafka } = require('kafkajs')
+var fs = require('fs');
+var axios = require('axios');
+
+
+class simpleConsumer{
+
+     constructor(){
+         this.tenant_config     = JSON.parse(process.env.JSON_TENANT_CONFIG ? process.env.JSON_TENANT_CONFIG : "")
+         this.kafka_topic_from  = this.tenant_config['streams'][process.env.KAFKA_TOPIC_FROM]['read'] 
+         this.client_id         = process.env.MESOS_TASK_ID
+     }
+
+     isConfigValid(){
+        var v = false;
+        if (
+            this.tenant_config !== undefined &&
+            this.tenant_config['brokers'] !== undefined &&
+            this.tenant_config['streams'] !== undefined &&
+            this.tenant_config['private_consumer_groups'] !== undefined &&
+            this.tenant_config['shared_consumer_groups'] !== undefined &&
+            this.kafka_topic_from !== undefined && this.kafka_topic_from !== "" &&
+            this.client_id !==undefined
+    
+        ){
+            v = true
+        }
+        return v;
+    }
+
+     async startApp(){
+
+        if(this.isConfigValid()){
+            this.kafka_config = {
+                clientId: this.client_id,
+                brokers: this.tenant_config['brokers'],
+                initialRetryTime: 100,
+                retries: 10,
+                ssl: {
+                    rejectUnauthorized: false,
+                    ca: fs.readFileSync('/home/node/app/ca.crt'),
+                    cert: fs.readFileSync('/home/node/app/client.crt'),
+                    key: fs.readFileSync('/home/node/app/client.key')
+                },
+            }
+
+            this.connector = new Kafka(this.kafka_config)
+
+            //read stream
+            this.consumer =  this.connector.consumer({groupId: this.tenant_config['private_consumer_groups'][1]})
+            await this.consumer.connect()
+            await this.consumer.subscribe({topic: new RegExp(this.kafka_topic_from)})
+
+            await this.consumer.run({
+
+                eachMessage: async ({topic, partition, message}) => {
+                   
+                }
+            })
+        }    
+     }
+};
+
+var app = new simpleConsumer()
+app.startApp()
